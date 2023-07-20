30d67748f94e6a07701fdfac831ffc2aa88a9925
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-20 18:35:37 +0200

    added Movie model in PageController in Guest folder

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+
 class PageController extends Controller
 {
    public function getMovies(){
