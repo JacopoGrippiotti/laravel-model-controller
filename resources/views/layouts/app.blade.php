4a8714107745d25968efade7026861735c84e676
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-20 18:36:25 +0200

    included partials in app.blade.php file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel App')</title>
+    <link rel="stylesheet" href="~resources/scss/app.scss">
  </head>
+ 
  <body>
+   
+   @include('partials.header')
+   
    <main>
       @yield('main-content')
    </main>
-
+   
+   @include('partials.footer')
    @vite('resources/js/app.js')
  </body>
 
