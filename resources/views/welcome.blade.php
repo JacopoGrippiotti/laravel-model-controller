a2159aef9d3e80514bbb053076955bb8b3601982
Jacopo Grippiotti <126581682+JacopoGrippiotti@users.noreply.github.com>
2023-07-20 15:28:29 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('main-content')
+    <h1>
+        Welcome
+    </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    ciaone
+                </h3>
+            </div>
+        </div>
+    </div>
+@endsection
+
